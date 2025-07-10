75156e0f43176475263ddafe8f278e6e26a73c92
Diego Apolo <diegoapolo2011@gmail.com>
2025-07-10 15:32:01 -0500

    ✨  feat(favorites): add favorites logic with Redux Toolkit and persistence

--- src/pages/favorites/index.tsx
+++ src/pages/favorites/index.tsx
@@ -1,5 +1,23 @@
+import { Atom } from '@/shared/ui';
+import type { RootState } from '@/store';
+import { clearFavorites } from '@/store/favoritesSlice';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+
 function Favorites() {
-  return <div className="text-primary">Favorites</div>;
+  const dispatch = useDispatch();
+  const favorites = useSelector((state: RootState) => state.favorites.favorites);
+
+  return (
+    <div className="text-primary">
+      {favorites?.map((launch) => (
+        <li key={launch.id}>
+          <strong>{launch.missionName}</strong> ({launch.rocketName}) – {launch.launchDate}
+        </li>
+      ))}
+      <Atom.Button onClick={() => dispatch(clearFavorites())}>Eliminar Todos</Atom.Button>
+    </div>
+  );
 }
 
 export default Favorites;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,28 @@
+import type { LaunchAdapter } from '@/domain/models';
 import { useGetLaunchesQuery } from '@/services';
+import { Atom } from '@/shared/ui';
+import type { RootState } from '@/store';
+import { addFavorite, removeFavorite } from '@/store/favoritesSlice';
+import { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 function Home() {
+  const dispatch = useDispatch();
   const { data: launches, isLoading, isError } = useGetLaunchesQuery({ limit: 5, offset: 0 });
+  const favorites = useSelector((state: RootState) => state.favorites.favorites);
+
+  const isFavorite = useCallback(
+    (id: string) => favorites.some((fav) => fav.id === id),
+    [favorites]
+  );
+
+  const handleAddOrRemoveFavoriteLaunch = (launch: LaunchAdapter) => {
+    if (isFavorite(launch.id)) {
+      dispatch(removeFavorite(launch.id));
+    } else {
+      dispatch(addFavorite(launch));
+    }
+  };
 
   if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>Error loading launches</div>;
@@ -13,6 +34,12 @@ function Home() {
         {launches?.map((launch) => (
           <li key={launch.id}>
             <strong>{launch.missionName}</strong> ({launch.rocketName}) – {launch.launchDate}
+            <Atom.Button
+              variant={'outline'}
+              onClick={() => handleAddOrRemoveFavoriteLaunch(launch)}
+            >
+              {isFavorite(launch.id) ? 'ELIMINAR' : 'AGREGAR'}
+            </Atom.Button>
           </li>
         ))}
       </ul>
